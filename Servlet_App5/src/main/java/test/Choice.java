f3af700c9e3bfcc3108036077e427a5fcb3f87d7
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2024-12-28 12:46:04 +0530

    Advance_Java uploaded

---
+++ Servlet_App5/src/main/java/test/Choice.java
@@ -0,0 +1,11 @@
+package test;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+@WebServlet("/choice")
+public class Choice extends GenericServlet{
+	public void service(ServletRequest req,ServletResponse res) throws ServletException,IOException
+	{
+		
+	}
+}
